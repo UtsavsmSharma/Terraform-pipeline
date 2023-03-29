8725001ffe9dffab29296c6fd6a3c5a81d908ca3
Utsav sharma <73983697+UtsavsmSharma@users.noreply.github.com>
2023-03-29 09:12:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ provider "aws" {
 resource "aws_instance" "code_pip_instance" {
   ami           = "ami-0d758c1134823146a"
   instance_type = "t2.micro"
+  region= "us-west-2"
   key_name = "key_pair_name"
   tags = {
     Name = "pipeline"
