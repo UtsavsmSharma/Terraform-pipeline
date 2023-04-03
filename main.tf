1f59b3d48b7f0bcc4d645b01cc96c5b8c9a9aa52
Utsav sharma <73983697+UtsavsmSharma@users.noreply.github.com>
2023-04-03 13:59:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,31 +2,31 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# # S3 Bucket resource
-# resource "aws_s3_bucket" "example_bucket" {
-#   bucket = "demotxchd"
-#   acl    = "private"
-# }
+# S3 Bucket resource
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "demotxchd2"
+  acl    = "private"
+}
 
 # EC2 Instance resource
-resource "aws_instance" "example_instance" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
-  key_name      = "tf"
+# resource "aws_instance" "example_instance" {
+#   ami           = "ami-00c39f71452c08778"
+#   instance_type = "t2.micro"
+#   key_name      = "tf"
 
-  # Attach the instance to a security group that allows SSH access
-  security_groups = ["ssh-access"]
+#   # Attach the instance to a security group that allows SSH access
+#   security_groups = ["ssh-access"]
 
-  # Provision the instance with a script
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!" > index.html
-              nohup python -m SimpleHTTPServer 80 &
-              EOF
+#   # Provision the instance with a script
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World!" > index.html
+#               nohup python -m SimpleHTTPServer 80 &
+#               EOF
 
-  # Attach an EBS volume to the instance
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    volume_size = 100
-  }
-}
+#   # Attach an EBS volume to the instance
+#   ebs_block_device {
+#     device_name = "/dev/sdh"
+#     volume_size = 100
+#   }
+# }
