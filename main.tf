b3bf148b82aa2fdbabb67f4e67beaa2677c361a0
Utsav sharma <73983697+UtsavsmSharma@users.noreply.github.com>
2023-04-03 13:23:02 +0530

    updated maint.tf

--- main.tf
+++ main.tf
@@ -8,25 +8,25 @@ resource "aws_s3_bucket" "example_bucket" {
   acl    = "private"
 }
 
-# EC2 Instance resource
-resource "aws_instance" "example_instance" {
-  ami           = "ami-00c39f71452c08778"
-  instance_type = "t2.micro"
-  key_name      = "tf"
+# # EC2 Instance resource
+# resource "aws_instance" "example_instance" {
+#   ami           = "ami-00c39f71452c08778"
+#   instance_type = "t2.micro"
+#   key_name      = "tf"
 
-  # Attach the instance to a security group that allows SSH access
-  security_groups = ["ssh-access"]
+#   # Attach the instance to a security group that allows SSH access
+#   security_groups = ["ssh-access"]
 
-  # Provision the instance with a script
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World!" > index.html
-              nohup python -m SimpleHTTPServer 80 &
-              EOF
+#   # Provision the instance with a script
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World!" > index.html
+#               nohup python -m SimpleHTTPServer 80 &
+#               EOF
 
-  # Attach an EBS volume to the instance
-  ebs_block_device {
-    device_name = "/dev/sdh"
-    volume_size = 100
-  }
-}
+#   # Attach an EBS volume to the instance
+#   ebs_block_device {
+#     device_name = "/dev/sdh"
+#     volume_size = 100
+#   }
+# }
