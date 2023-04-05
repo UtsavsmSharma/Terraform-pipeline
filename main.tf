b279e37af385f3293e53454d063b6e87fed6f4b7
Utsav sharma <73983697+UtsavsmSharma@users.noreply.github.com>
2023-04-05 10:30:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,50 @@
-provider "aws" {
-  region = "us-east-1"
+    protocol    = "tcp"# EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-00c39f71452c08778"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "example"
+  }
 }
-# S3 Bucket resource
-resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-bucket-13131"
+
+# S3 Bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "bucky8o08i87j"
   acl    = "private"
+
+  tags = {
+    Name = "example"
+  }
+}
+
+# VPC
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example"
+  }
+}
+
+# Security Group
+resource "aws_security_group" "example" {
+  name_prefix = "example-sg"
+  vpc_id      = aws_vpc.example.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "example"
+  }
 }
